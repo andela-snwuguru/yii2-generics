a06cbfbf7387a54fbdab0892d74cb89e84f6b5ad
Nwuguru Sunday <sunday.nwuguru@andela.com>
2016-08-21 15:08:23 +0100

    Add YedCreateViewController

---
+++ controllers/YedCreateViewController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace digitech\yiigenerics\controllers;
+
+
+trait YedCreateViewController
+{
+    use \digitech\yiigenerics\controllers\YedController;
+    use \digitech\yiigenerics\controllers\YedCreateView;
+}
